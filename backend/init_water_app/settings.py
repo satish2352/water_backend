faa125f82be738cbeb75a893c005ed6dd9dc6e94
vikas <vikas>
2023-08-24 15:01:32 +0530

    coment dbrouter

--- backend/init_water_app/settings.py
+++ backend/init_water_app/settings.py
@@ -190,7 +190,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # settings.py
 
-DATABASE_ROUTERS = ['connection.router.connectionRouter']
+# DATABASE_ROUTERS = ['connection.router.connectionRouter']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
