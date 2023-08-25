3f363212ec8366b519d4cc791c5aeec9022cc21d
vikas <vikas>
2023-08-25 16:37:35 +0530

    chenges from assim sir in meeting11

--- backend/connection/views.py
+++ backend/connection/views.py
@@ -215,7 +215,8 @@ class MqttClient:
             print("doublicate_panel_id: ",doublicate_panel_id)
         if atmid is not None:
             doublicate_atm_id=Device.objects.filter(serial_no3 =atmid).filter(~Q(site_id = site_ids))
-            print("doublicate_atm_id:",doublicate_atm_id)
+            for aaa in doublicate_atm_id:
+                print("doublicate_atm_id:",aaa.serial_no3)
         if doublicate_panel_id is not None or doublicate_atm_id is not None:
           logger.info("Doublicate device found")
         
