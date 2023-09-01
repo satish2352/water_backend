0bcf3df46a6ec4a6690b4cb9d4e7854b22b5b842
Satish <satishaurange@sumagoinfotech.com>
2023-09-01 16:54:59 +0530

    changes in login api company id  new  updated new

--- backend/iwater/views/signup_mgmnt.py
+++ backend/iwater/views/signup_mgmnt.py
@@ -83,14 +83,14 @@ def user_login(request):
                             'contact_no': logged_user.phone,
                             'site_name':sitename,
                             'company_name':company_name.company_name,
-                            'company_id':user.company_id,
+                            'company_id':logged_user.company_id,
                             'user_id':userfinder
                         }
                     }
                     if role == "super_admin":
                         company_details = Company.objects.get(id=logged_user.company_id)
                         user_data["user"]["company_name"] = company_details.company_name
-                        user_data["user"]["company_id"] = user.company_id,
+                        user_data["user"]["company_id"] = logged_user.company_id,
                         user_data["user"]["gst_no"] = company_details.gst_no
                         user_data["user"]["address1"] = company_details.address1
                         user_data["user"]["address2"] = company_details.address2
