ec1fa2dafec0624c931b80b2a67f673ee811a73f
Satish <satishaurange@sumagoinfotech.com>
2023-09-04 13:08:09 +0530

    changes for the device _id  new

--- backend/connection/views.py
+++ backend/connection/views.py
@@ -7839,7 +7839,7 @@ class tap4settingViewset(viewsets.ModelViewSet):
                 print("company_id is**** :", request.user.company_id)
                 # dinfo=device_info.objects.filter(componant_name=value_list[2],unit_type=value_list[1],company_id=value_list[0])
                 global deviceid
-                site_obj = dinfo=device_info.objects.filter(componant_name=value_list[2],unit_type=value_list[1],company_id=request.user.company_id).first()
+                site_obj = dinfo=device_info.objects.filter(unit_type=value_list[1],company_id=request.user.company_id).first()
                 print("site_obj ",site_obj)
                 deviceid=site_obj.Device_id
                 print("deviceid ",deviceid)
@@ -7848,7 +7848,7 @@ class tap4settingViewset(viewsets.ModelViewSet):
                         del data_dict[key]
                 for key in data_dict:
                     data_dict[key] = str(data_dict[key])
-                mqttc.publish(f'wc1/{did}/chgset/{cmpname}',str(data_dict).replace(' ',''))
+                mqttc.publish(f'wc1/{did}/chgset/tap4',str(data_dict).replace(' ',''))
                 dd=dateandtime()
                 e=f"{dd[0]}-{dd[1]}-{dd[2]} {dd[3]}:{dd[4]}:{dd[5]} tap4 settings change has been requested - pulse1:{value_list[3]}, pulse2:{value_list[4]}, pulse3:{value_list[5]}, pulse4:{value_list[6]}"
                 erro=Errors.objects.create(device_id=deviceid,e_discriptions=e,service='tap4',year=dd[0],month=dd[1],day=dd[2],hour=dd[3],minit=dd[4],second=dd[5])
