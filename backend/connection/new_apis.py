f0a3857f54294ec24ee27ad413f57a95f11dcc72
Satish <satishaurange@sumagoinfotech.com>
2023-09-06 19:46:22 +0530

    Created new Api2 updated

--- backend/connection/new_apis.py
+++ backend/connection/new_apis.py
@@ -33,29 +33,31 @@ def newtap1settingViewset(request):
             value_list=list(data_dict.values())
             print("value_list value_list",value_list)
             print("value_list[0] ",value_list[0])
-            dinfo=device_info.objects.filter(unit_type=value_list[0],company_id=request.user.company_id).first()
-            print("dinfo dinfo",dinfo)
-            obj = tap1_setting.objects.create(**data_dict)
-            for key in unwanted_keys:
-                        if key in data_dict.keys():
-                            del data_dict[key]
-            
-            deviceid = None
-            deviceid=dinfo.Device_id
-            print("deviceid ",deviceid)
-            if deviceid:
-                mqttc.publish(f'wc1/{deviceid}/chgset/tap1',str(data_dict).replace(' ',''))
-                dd=dateandtime()
-                e=f"{dd[0]}-{dd[1]}-{dd[2]} {dd[3]}:{dd[4]}:{dd[5]} tap1 settings change has been requested - pulse1:{value_list[3]}, pulse2:{value_list[4]}, pulse3:{value_list[5]}, pulse4:{value_list[6]}"
-                erro=Errors.objects.create(device_id=deviceid,e_discriptions=e,service='tap1',year=dd[0],month=dd[1],day=dd[2],hour=dd[3],minit=dd[4],second=dd[5])
-                erro.save()
+            dinfo = device_info.objects.filter(unit_type=value_list[0],company_id=request.user.company_id).first()
+            if dinfo is not None:
+                print("dinfo dinfo",dinfo)
+                obj = tap1_setting.objects.create(**data_dict)
+                for key in unwanted_keys:
+                            if key in data_dict.keys():
+                                del data_dict[key]
+                
+                deviceid = None
+                deviceid=dinfo.Device_id
+                print("deviceid ",deviceid)
+                if deviceid:
+                    mqttc.publish(f'wc1/{deviceid}/chgset/tap1',str(data_dict).replace(' ',''))
+                    dd=dateandtime()
+                    print("dd dd ",dd)
+                    e=f"{dd[0]}-{dd[1]}-{dd[2]} {dd[3]}:{dd[4]}:{dd[5]} tap1 settings change has been requested - pulse1:{value_list[3]}, pulse2:{value_list[4]}, pulse3:{value_list[5]}, pulse4:{value_list[6]}"
+                    erro=Errors.objects.create(device_id=deviceid,e_discriptions=e,service='tap1',year=dd[0],month=dd[1],day=dd[2],hour=dd[3],minit=dd[4],second=dd[5])
+                    erro.save()
 
-                obj.unit_type = value_list[0]
-                obj.componant_name = value_list[1]
-                obj.device_id = deviceid
-                obj.company_id = request.user.company_id
-                obj.save()
-            return Response({"message": "NEW TAP-1 API 200"})
+                    obj.unit_type = value_list[0]
+                    obj.componant_name = value_list[1]
+                    obj.device_id = deviceid
+                    obj.company_id = request.user.company_id
+                    obj.save()
+                return Response({"message": "NEW TAP-1 API 200"})
         except Exception as e:
             print("Error in tap1setting ",e)    
 
