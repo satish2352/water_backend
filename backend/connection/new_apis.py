bd73c479d16f6d5e87f8ba31bb68ab36417fa30c
Satish <satishaurange@sumagoinfotech.com>
2023-09-06 19:27:16 +0530

    Created new Api2 Harshal updated new one updated one satish

--- backend/connection/new_apis.py
+++ backend/connection/new_apis.py
@@ -30,11 +30,12 @@ def newtap1settingViewset(request):
             print("request.user.company_id ",request.user.company_id)
             data_dict = json.loads(request.body)
             unwanted_keys = ["unit_type", "water_treatment","componant_name","site_name","device_id"]
+            
+            obj = tap1_setting.objects.create(**data_dict)
+            value_list=list(data_dict.values())
             for key in unwanted_keys:
                         if key in data_dict.keys():
                             del data_dict[key]
-            obj = tap1_setting.objects.create(**data_dict)
-            value_list=list(data_dict.values())
             print("value_list value_list",value_list)
             dinfo=device_info.objects.filter(unit_type=value_list[0],company_id=request.user.company_id).first()
             print("dinfo dinfo",dinfo)
