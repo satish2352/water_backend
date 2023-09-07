c8039683db9a99fe50b3ec32bf2355e6dd7d670b
Satish <satishaurange@sumagoinfotech.com>
2023-09-07 15:42:46 +0530

    changes for the updated at update api new

--- backend/connection/new_apis.py
+++ backend/connection/new_apis.py
@@ -179,6 +179,9 @@ def updated_disp_Tap1Viewset(request):
             data_sta = model_to_dict(qs_sta[0], exclude=fields_to_exclude) if qs_sta else {}
 
             qs_set = disp_tap1.objects.filter(device_id=did, message_type="updset").order_by('-id')[:1:1]
+            for i in qs_set:
+                print(i)
+                print(i.created_at)
             created_at_string = str(disp_tap1.created_at)
             disp_tap1.created_at = created_at_string
             data_set = model_to_dict(qs_set[0], exclude=fields_to_exclude) if qs_set else {}
