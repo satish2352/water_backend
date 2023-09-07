d39ac8d442373c7d6810f04c6fd9d0de1de97017
vikas <vikas>
2023-09-07 12:37:49 +0530

    chnges for api

--- backend/connection/new_apis.py
+++ backend/connection/new_apis.py
@@ -81,7 +81,7 @@ def newtap2settingViewset(request):
                     e=f"{dd[0]}-{dd[1]}-{dd[2]} {dd[3]}:{dd[4]}:{dd[5]} tap2 settings change has been requested - pulse1:{value_list[2]}, pulse2:{value_list[3]}, pulse3:{value_list[4]}, pulse4:{value_list[5]}"
                     erro=Errors.objects.create(device_id=deviceid,e_discriptions=e,service='tap2',year=dd[0],month=dd[1],day=dd[2],hour=dd[3],minit=dd[4],second=dd[5])
                     erro.save()
-                    
+
                     obj = tap2_setting.objects.create(**data_dict)
                     obj.unit_type = value_list[0]
                     obj.componant_name = value_list[1]
@@ -438,3 +438,46 @@ def atmsettingViewset(request):
                 return Response({"message": "NEW ATM API 200"})
         except Exception as e:
             print("Error in atmsetting ",e)    
+
+
+
+#cnd_sen
+@api_view(['POST'])
+def hppstateViewset(request):
+    if request.method == 'POST':
+        try:
+            print("request  ",request)
+            print("request body   ",request.user)
+            print("request.user.company_id ",request.user.company_id)
+            data_dict = json.loads(request.body)
+            unwanted_keys = ["unit_type", "water_treatment","company_id","componant_name","site_name","device_id"]
+            value_list=list(data_dict.values())
+            print("value_list value_list",value_list)
+            print("value_list[0] ",value_list[0])
+            dinfo = device_info.objects.filter(unit_type=value_list[0],company_id=request.user.company_id).first()
+            if dinfo is not None:
+                print("dinfo dinfo",dinfo)
+                obj = ampv1_state.objects.create(**data_dict)
+                for key in unwanted_keys:
+                    if key in data_dict.keys():
+                        del data_dict[key]
+                
+                deviceid = None
+                deviceid=dinfo.Device_id
+                print("deviceid ",deviceid)
+                if deviceid:
+                    mqttc.publish(f'wc1/{deviceid}/chgset/hpp',str(data_dict).replace(' ',''))
+                    dd=dateandtime()
+                    print("dd dd ",dd)
+                    e=f"{dd[0]}-{dd[1]}-{dd[2]} {dd[3]}:{dd[4]}:{dd[5]} hpp state change has been requested - pulse1:{value_list[2]}, pulse2:{value_list[3]}, pulse3:{value_list[4]}, pulse4:{value_list[5]}"
+                    erro=Errors.objects.create(device_id=deviceid,e_discriptions=e,service='hpp_state',year=dd[0],month=dd[1],day=dd[2],hour=dd[3],minit=dd[4],second=dd[5])
+                    erro.save()
+
+                    obj.unit_type = value_list[0]
+                    obj.componant_name = value_list[1]
+                    obj.device_id = deviceid
+                    obj.company_id = request.user.company_id
+                    obj.save()
+                return Response({"message": "NEW HPP STATE API 200"})
+        except Exception as e:
+            print("Error in HPP STATE  ",e)
