41e045f9e017230cfbfa6bc0f86e6e0c121a59f4
vikas <vikas>
2023-09-09 15:54:24 +0530

    changes for tap1 setting

--- backend/connection/new_apis.py
+++ backend/connection/new_apis.py
@@ -26,41 +26,93 @@ def dateandtime():
 def newtap1settingViewset(request):
     if request.method == 'POST':
         try:
-            print("request  ",request)
-            print("request body  ",request.user)
-            print("request.user.company_id ",request.user.company_id)
             data_dict = json.loads(request.body)
-            unwanted_keys = ["unit_type", "water_treatment","componant_name","site_name","device_id"]
-            value_list=list(data_dict.values())
-            print("value_list value_list",value_list)
-            print("value_list[0] ",value_list[0])
-            dinfo = device_info.objects.filter(unit_type=value_list[0],company_id=request.user.company_id).first()
+            value_list = data_dict
+           
+            dinfo = device_info.objects.filter(unit_type=value_list['unit_type'],company_id=request.user.company_id).first()
             if dinfo is not None:
-                print("dinfo dinfo",dinfo)
-                obj = tap1_setting.objects.create(**data_dict)
-                for key in unwanted_keys:
-                            if key in data_dict.keys():
-                                del data_dict[key]
-                
+
+                device_final_data = {}
+                device_final_data['p1'] = value_list['p1']
+                device_final_data['p2'] = value_list['p2']
+                device_final_data['p3'] = value_list['p3']
+                device_final_data['p4'] = value_list['p4']
+
+                for key, value in device_final_data.items():
+                    value = str(value)
+                    temp = value.isalnum()
+                    if  temp is not False:
+                        value.replace('"', "'")
+                        value.replace(' ','')
+                        device_final_data[key] = value
+                    else:
+                        device_final_data[key] = ''
+
                 deviceid = None
-                deviceid=dinfo.Device_id
-                print("deviceid ",deviceid)
+                deviceid = dinfo.Device_id
+
                 if deviceid:
-                    mqttc.publish(f'wc1/{deviceid}/chgset/tap1',str(data_dict).replace(' ',''))
+                    mqttc.publish(f'wc1/{deviceid}/chgset/tap1',str(device_final_data).replace(' ',''))
                     dd=dateandtime()
                     print("dd dd ",dd)
-                    e=f"{dd[0]}-{dd[1]}-{dd[2]} {dd[3]}:{dd[4]}:{dd[5]} tap1 settings change has been requested - pulse1:{value_list[2]}, pulse2:{value_list[3]}, pulse3:{value_list[4]}, pulse4:{value_list[5]}"
+                    e=f"{dd[0]}-{dd[1]}-{dd[2]} {dd[3]}:{dd[4]}:{dd[5]} tap1 settings change has been requested - pulse1:{value_list['p1']}, pulse2:{value_list['p2']}, pulse3:{value_list['p3']}, pulse4:{value_list['p4']}"
                     erro=Errors.objects.create(device_id=deviceid,e_discriptions=e,service='tap1',year=dd[0],month=dd[1],day=dd[2],hour=dd[3],minit=dd[4],second=dd[5])
                     erro.save()
-
-                    obj.unit_type = value_list[0]
-                    obj.componant_name = value_list[1]
-                    obj.device_id = deviceid
-                    obj.company_id = request.user.company_id
-                    obj.save()
-                return Response({"message": "NEW TAP-1 API 200"})
+                    try:
+                        value_list_final = {}
+                        value_list_final['p1'] = value_list['p1']
+                        value_list_final['p2'] = value_list['p2']
+                        value_list_final['p3'] = value_list['p3']
+                        value_list_final['p4'] = value_list['p4']
+                        value_list_final['componant_name'] = 'tap1'
+                        value_list_final['device_id'] = deviceid
+                        value_list_final['company_id'] = request.user.company_id
+                        tap1_setting.objects.create(**value_list_final)
+                        return Response({"message": "NEW TAP-1 API 200"})
+                    except Exception as e:
+                        print("Error in tap1setting ",e) 
         except Exception as e:
             print("Error in tap1setting ",e)    
+
+
+
+    # if request.method == 'POST':
+    #     try:
+    #         print("request  ",request)
+    #         print("request body  ",request.user)
+    #         print("request.user.company_id ",request.user.company_id)
+    #         data_dict = json.loads(request.body)
+    #         unwanted_keys = ["unit_type", "water_treatment","componant_name","site_name","device_id"]
+    #         value_list=list(data_dict.values())
+    #         print("value_list value_list",value_list)
+    #         print("value_list[0] ",value_list[0])
+    #         dinfo = device_info.objects.filter(unit_type=value_list[0],company_id=request.user.company_id).first()
+    #         if dinfo is not None:
+    #             print("dinfo dinfo",dinfo)
+    #             obj = tap1_setting.objects.create(**data_dict)
+    #             for key in unwanted_keys:
+    #                         if key in data_dict.keys():
+    #                             del data_dict[key]
+                
+    #             deviceid = None
+    #             deviceid=dinfo.Device_id
+    #             print("deviceid ",deviceid)
+    #             if deviceid:
+    #                 mqttc.publish(f'wc1/{deviceid}/chgset/tap1',str(data_dict).replace(' ',''))
+    #                 dd=dateandtime()
+    #                 print("dd dd ",dd)
+    #                 e=f"{dd[0]}-{dd[1]}-{dd[2]} {dd[3]}:{dd[4]}:{dd[5]} tap1 settings change has been requested - pulse1:{value_list[2]}, pulse2:{value_list[3]}, pulse3:{value_list[4]}, pulse4:{value_list[5]}"
+    #                 erro=Errors.objects.create(device_id=deviceid,e_discriptions=e,service='tap1',year=dd[0],month=dd[1],day=dd[2],hour=dd[3],minit=dd[4],second=dd[5])
+    #                 erro.save()
+
+    #                 obj.unit_type = value_list[0]
+    #                 obj.componant_name = value_list[1]
+    #                 obj.device_id = deviceid
+    #                 obj.company_id = request.user.company_id
+    #                 obj.save()
+    #             return Response({"message": "NEW TAP-1 API 200"})
+    #     except Exception as e:
+    #         print("Error in tap1setting ",e)    
 
 @api_view(['POST'])
 def newtap2settingViewset(request):
