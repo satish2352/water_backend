83d922e817a55757a78ce8bb5b2a2bf3b39d951c
vikas <vikas>
2023-09-11 15:46:52 +0530

    changes for all commponents arrtribute initial value is None

--- backend/iwater/views/user_mgmnt.py
+++ backend/iwater/views/user_mgmnt.py
@@ -320,6 +320,10 @@ def add_user(request):
                     # user.site_limit = site_limit
                     user.company = company
                     user.added_by = request.user
+                    # site_obj = Site.objects.get(company_id=request.user.company_id)
+                    # if site_obj:
+                    #     if  role=="operator":
+                    
                     user.save()
 
                     logger.info("saved user details")
