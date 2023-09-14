310287c8d0ff51d9cf7a5879159b4ba2cffe9914
Satish <satishaurange@sumagoinfotech.com>
2023-09-14 14:17:17 +0530

    changes for updated new rwp new

--- backend/connection/new_api_harshal.py
+++ backend/connection/new_api_harshal.py
@@ -214,13 +214,13 @@ def newupdated_treat_rwp_Viewset(request):
             did = dinfo.Device_id
             qs_sta = treat_rwp.objects.filter(device_id=did, message_type="updsta").values("sts","crt","olc","drc","spn","created_at","updated_at").order_by('-id')[:1:1]
             print("qs_sta",qs_sta)
-            if qs_sta is not None:
+            if qs_sta:
                 qs_sta_final = qs_sta[0]
             else:
                 qs_sta_final = {}
             print("qs_sta_final",qs_sta_final)
             qs_set = treat_rwp.objects.filter(device_id=did, message_type="updset").values("sts","crt","olc","drc","spn","created_at","updated_at").order_by('-id')[:1:1]
-            if qs_set is not None:
+            if qs_set:
                 qs_set_final = qs_set[0]
             else:
                 qs_set_final = {}
