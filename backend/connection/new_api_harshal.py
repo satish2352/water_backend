d7dccda59074b1d626674098c5944c3ad8e65d66
Satish <satishaurange@sumagoinfotech.com>
2023-09-14 17:42:35 +0530

    changes for the apmv1 setting null set for all the fileds if no data from frontend updated

--- backend/connection/new_api_harshal.py
+++ backend/connection/new_api_harshal.py
@@ -415,9 +415,8 @@ def ampv1settingViewset(request):
                     try:
                         value_list_final = {}
                         if value_list['srt']:
-                        data_srt  = value_list['srt']:
-                        # print("data_srt.replace("", "")",data_srt.replace(":", ""))
-                        value_list_final['srt'] = data_srt.replace(":", "")
+                            data_srt  = value_list['srt']:
+                            value_list_final['srt'] = data_srt.replace(":", "")
 
                         if value_list['bkt']:
                             value_list_final['bkt'] = value_list['bkt'] 
